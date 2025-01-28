eb66a2265fbd4a69ce6f6d72ae524364d6a7f666
JCollantesVega <collantes.jantonio@gmail.com>
2025-01-28 04:24:03 +0100

    Input control stablished

--- src/proyectoACD/KartApp.java
+++ src/proyectoACD/KartApp.java
@@ -15,23 +15,32 @@ public class KartApp {
 	
 	private int menuInicio()
 	{
-		System.out.println("Bienvenido!");
-		System.out.println("1- Iniciar sesión");
-		System.out.println("2- Crear nuevo usuario");
-		System.out.println("3- Salir");
-		System.out.println("Elija una opción: ");
-		int opt = sc.nextInt();
 		
-		return opt;
+		do //bucle que controla que el usuario introduzca un número entero
+		{
+			System.out.println("Bienvenido!");
+			System.out.println("1- Iniciar sesión");
+			System.out.println("2- Crear nuevo usuario");
+			System.out.println("3- Salir");
+			System.out.println("Elija una opción: ");			
+		}while(!sc.hasNextInt());
+		
+		return sc.nextInt();
 	}
 	
-	private void menUsuario(String nombreUsuario)
+	private int menuUsuario(String nombreUsuario)
 	{
-		System.out.println("Bienvenido " + nombreUsuario + ", ¿qué desea hacer?");
-		System.out.println("1-Unirse a una sesión");
-		System.out.println("2-Ver mis sesiones anteriores");
-		System.out.print("3-Salir");
-		int opt = sc.nextInt();
+		do //bucle que controla que el usuario introduzca un número entero
+		{
+			System.out.println("Bienvenido " + nombreUsuario + ", ¿qué desea hacer?");
+			System.out.println("1-Unirse a una sesión");
+			System.out.println("2-Ver mis sesiones anteriores");
+			System.out.println("3-Cambiar información personal");
+			System.out.println("4-Darse de baja");
+			System.out.println("5-Cerrar sesión");			
+		}while(!sc.hasNextInt());
+		
+		return sc.nextInt();
 	}
 	
 }
