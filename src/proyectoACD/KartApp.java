d54fa6c663bbc68e657f100f867abfbe106189b5
JCollantesVega <collantes.jantonio@gmail.com>
2025-01-29 04:12:23 +0100

    Added two new menus

--- src/proyectoACD/KartApp.java
+++ src/proyectoACD/KartApp.java
@@ -12,35 +12,66 @@ public class KartApp {
 		gDb = new GestorBaseDatos(); 
 	}
 	
-	
-	private int menuInicio()
+	public void appInit()
 	{
-		
-		do //bucle que controla que el usuario introduzca un número entero
+		int opc = -1;
+		while(opc!=3)
 		{
-			System.out.println("Bienvenido!");
-			System.out.println("1- Iniciar sesión");
-			System.out.println("2- Crear nuevo usuario");
-			System.out.println("3- Salir");
-			System.out.println("Elija una opción: ");			
-		}while(!sc.hasNextInt());
-		
-		return sc.nextInt();
+			menuInicio();
+			opc = sc.nextInt(); sc.nextLine();
+			switch(opc)
+			{
+				case 1:
+					menuUsuario(inicioSesion());
+					break;
+				case 2:
+					menuAdmin();
+					break;
+				case 3:
+					break;
+				default:
+					break;
+			}
+		}
+			
 	}
 	
-	private int menuUsuario(String nombreUsuario)
+	private void menuInicio()
 	{
-		do //bucle que controla que el usuario introduzca un número entero
-		{
-			System.out.println("Bienvenido " + nombreUsuario + ", ¿qué desea hacer?");
-			System.out.println("1-Unirse a una sesión");
-			System.out.println("2-Ver mis sesiones anteriores");
-			System.out.println("3-Cambiar información personal");
-			System.out.println("4-Darse de baja");
-			System.out.println("5-Cerrar sesión");			
-		}while(!sc.hasNextInt());
-		
-		return sc.nextInt();
+		System.out.println("Bienvenido!");
+		System.out.println("1- Iniciar sesión");
+		System.out.println("2- Crear nuevo usuario");
+		System.out.println("3- Salir");
+		System.out.println("Elija una opción: ");
+	}
+	
+	private void menuUsuario(String nombreUsuario)
+	{
+
+		System.out.println("Bienvenido " + nombreUsuario + ", ¿qué desea hacer?");
+		System.out.println("1-Unirse a una sesión");
+		System.out.println("2-Ver mis sesiones anteriores");
+		System.out.println("3-Cambiar información personal");
+		System.out.println("4-Darse de baja");
+		System.out.println("5-Cerrar sesión");
+
+	}
+	
+	private void menuAdmin()
+	{
+		System.out.println("Bienvenido, ¿qué desea hacer?");
+		System.out.println("1-Crear sesión");
+		System.out.println("2-Ver mis sesiones anteriores");
+		System.out.println("3-Cambiar información personal");
+		System.out.println("4-Darse de baja a un usuario");
+		System.out.println("5-Cerrar sesión");
+	}
+	
+	private String inicioSesion()
+	{
+		System.out.print("Introduzca su dni: ");
+
+		return sc.nextLine();
 	}
 	
 }
